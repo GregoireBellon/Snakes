c6b0c3f9e44b1cab72b89bff5cb8654948182e24
Marc-Eli CROAIN <mcroain@gmail.com>
2023-04-03 21:10:44 +0200

    conflict

--- SnakeClient/src/tests/TestRun.java
+++ SnakeClient/src/tests/TestRun.java
@@ -29,47 +29,18 @@ public class TestRun {
 		
 		
 		RemoteSnakeController controller = new RemoteSnakeController();		
-<<<<<<< dev
+
 		ConnexionView view = new ConnexionView(controller);
-<<<<<<< HEAD
-	
-=======
->>>>>>> refs/heads/gestion-auth
-=======
+
 //		ConnexionView view = new ConnexionView(controller);
-	
-		controller.setConnexion("localhost:44444", "jhon", "jhon");
->>>>>>> eccf864 Ajout de l'authentification
-		
-<<<<<<< dev
-<<<<<<< HEAD
-		view.setVisible(true);
-		while(view.isVisible()) {
-			if(controller.setConnexion("localhost:44464", "jhon", "jhon")) {
-				view.setVisible(false);
-			}
-		}
-		view.dispose();
-		controller.play();
-=======
->>>>>>> refs/heads/gestion-auth
-=======
-		controller.play();
->>>>>>> eccf864 Ajout de l'authentification
-		
-<<<<<<< dev
-<<<<<<< HEAD
-    
-=======
+
 		//controller.setConnexion("localhost:44444","laura_jones@mail.fr", "laulau");
 		
 		//controller.play();
 		
->>>>>>> refs/heads/gestion-auth
-		
-=======
+
 //		view.setVisible(true);
->>>>>>> eccf864 Ajout de l'authentification
+
 		
 		
 	}
